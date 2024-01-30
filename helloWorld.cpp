f25bf4b529956f4a3075c8069778ea55ea6528ea
Dionisio Aceves <10143@portalcedi.edu.mx>
2024-01-30 13:43:44 -0600

    aña

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
+    int edad;
     cout << "hello world"<<endl;
+    cin >> edad;
+    if (edad>=18)
+    {
+     cout << "eres mayor de edad"<<endl;
+    }
     return 0;
 }
